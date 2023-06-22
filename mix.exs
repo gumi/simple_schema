ef56408402b4360bd3cf20fe80cd0f7dc7432da9
melpon <shigemasa7watanabe+github@gmail.com>
2023-06-22 11:47:22 +0900

    1.2.2 リリース

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleSchema.Mixfile do
   def project do
     [
       app: :simple_schema,
-      version: "1.2.1",
+      version: "1.2.2",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: "Validate JSON and store to a specified data structure",
